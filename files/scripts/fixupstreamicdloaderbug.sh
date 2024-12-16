0ebb611d9f363a1c9c79993618f57db7010ce106
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-12-16 00:09:38 -0800

    Update fixupstreamicdloaderbug.sh

--- files/scripts/fixupstreamicdloaderbug.sh
+++ files/scripts/fixupstreamicdloaderbug.sh
@@ -6,6 +6,7 @@ set -oue pipefail
 # mitigate upstream packaging bug: https://bugzilla.redhat.com/show_bug.cgi?id=2332429
 # swap the incorrectly installed OpenCL-ICD-Loader for ocl-icd, the expected package
 rpm-ostree override replace \
+  --from repo='fedora' \
   --experimental \
   --remove=OpenCL-ICD-Loader \
   ocl-icd \
