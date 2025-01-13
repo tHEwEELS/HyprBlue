2131901c5445c1b2abdcfd0a404c6171850eb3cb
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-01-12 17:11:45 -0800

    fix

--- modules/wayblue-signing/wayblue-signing.sh
+++ modules/wayblue-signing/wayblue-signing.sh
@@ -3,7 +3,8 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
-CONTAINER_DIR="/etc/containers"
+CONTAINER_DIR="/usr/etc/containers"
+ETC_CONTAINER_DIR="/etc/containers"
 MODULE_DIRECTORY="${MODULE_DIRECTORY:-"/tmp/modules"}"
 IMAGE_NAME_FILE="${IMAGE_NAME//\//_}"
 IMAGE_REGISTRY_TITLE=$(echo "$IMAGE_REGISTRY" | cut -d'/' -f2-)
@@ -15,13 +16,28 @@ if ! [ -d "$CONTAINER_DIR" ]; then
     mkdir -p "$CONTAINER_DIR"
 fi
 
+if ! [ -d "$ETC_CONTAINER_DIR" ]; then
+    mkdir -p "$ETC_CONTAINER_DIR"
+fi
+
 if ! [ -d $CONTAINER_DIR/registries.d ]; then
    mkdir -p "$CONTAINER_DIR/registries.d"
+fi
+
+if ! [ -d $ETC_CONTAINER_DIR/registries.d ]; then
+   mkdir -p "$ETC_CONTAINER_DIR/registries.d"
 fi
 
 if ! [ -d "/usr/etc/pki/containers" ]; then
     mkdir -p "/usr/etc/pki/containers"
 fi
+
+if ! [ -d "/etc/pki/containers" ]; then
+    mkdir -p "/etc/pki/containers"
+fi
+
+cp "$MODULE_DIRECTORY/wayblue-signing/policy.json" $CONTAINER_DIR/policy.json
+cp "$MODULE_DIRECTORY/wayblue-signing/policy.json" $ETC_CONTAINER_DIR/policy.json
 
 # covering our bases here since /usr/etc is technically unsupported, reevaluate once bootc is the primary deployment tool
 cp "/etc/pki/containers/$IMAGE_NAME.pub" "/usr/etc/pki/containers/$IMAGE_REGISTRY_TITLE.pub"
@@ -48,5 +64,7 @@ cp POLICY.tmp /usr/etc/containers/policy.json
 cp POLICY.tmp /etc/containers/policy.json
 rm POLICY.tmp
 
-mv "$MODULE_DIRECTORY/wayblue-signing/registry-config.yaml" "$CONTAINER_DIR/registries.d/$IMAGE_REGISTRY_TITLE.yaml"
-sed -i "s ghcr.io/IMAGENAME $IMAGE_REGISTRY g" "$CONTAINER_DIR/registries.d/$IMAGE_REGISTRY_TITLE.yaml"
+sed -i "s ghcr.io/IMAGENAME $IMAGE_REGISTRY g" "$MODULE_DIRECTORY/wayblue-signing/registry-config.yaml"
+cp "$MODULE_DIRECTORY/wayblue-signing/registry-config.yaml" "$CONTAINER_DIR/registries.d/$IMAGE_REGISTRY_TITLE.yaml"
+cp "$MODULE_DIRECTORY/wayblue-signing/registry-config.yaml" "$ETC_CONTAINER_DIR/registries.d/$IMAGE_REGISTRY_TITLE.yaml"
+rm "$MODULE_DIRECTORY/wayblue-signing/registry-config.yaml"
